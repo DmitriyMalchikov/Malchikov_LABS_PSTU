2c5cdb08682440ba364e572a663925376ba539b9
DmitriyMalchikov <130307065+DmitriyMalchikov@users.noreply.github.com>
2023-05-18 11:31:17 +0300

    Add files via upload

---
+++ LabsClass7/Project29/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include "List.h"
+#include "Pair.h"
+#include <Windows.h>
+
+int main() {
+	SetConsoleCP(1251); SetConsoleOutputCP(1251);
+	Pair p1(2, 2.5);
+	List<Pair> a(3, p1);
+	cout << "first list: ";
+	cout << a << endl;
+	cout << "creating personal list: ";
+	cin >> a;
+	cout << a << endl;
+	List<Pair> b(a);
+	cout << "copied list: ";
+	cout << b << endl;
+	List<Pair> c = a * b;
+	cout << "a * b\n" << c << endl;
+	Pair num;
+	cout << "Enter number to add:\n"; cin >> num;
+	b.pushback(num);
+	for (Iterator<Pair> iter = b.first(); iter != b.last(); ++iter)
+		cout << *iter << '\n';
+	system("pause");
+	return 0;
+}
